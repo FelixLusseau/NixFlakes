0074b3c7c0d42e515598d2b33e56568ad936d43e
FelixLusseau <felix.lusseau@gmail.com>
2024-12-10 00:09:51 +0100

    theme test next

--- modules/gui/home-manager/theme.nix
+++ modules/gui/home-manager/theme.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation rec {
     version = "1.0";
     dontBuild = true;
     installPhase = ''
-        mkdir -p $out/share/plasma/themes
-        cp -aR $src/Vivid\ Plasma\ Themes/ $out/share/plasma/themes/Vivid-Dark-Plasma/
+        mkdir -p $out/share/plasma
+        cp -aR $src/Vivid\ Plasma\ Themes/ $out/share/plasma/Vivid-Dark-Plasma/
     '';
     src = fetchFromGitHub {
         owner = "L4ki";
