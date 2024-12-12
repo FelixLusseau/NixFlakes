e7bf15b1620f701cbea863de0ec3d484ab39dd43
FelixLusseau <felix.lusseau@gmail.com>
2024-12-12 22:53:18 +0100

    window decoration

--- modules/gui/home-manager/theme/gently.nix
+++ modules/gui/home-manager/theme/gently.nix
@@ -12,6 +12,6 @@ stdenv.mkDerivation rec {
         owner = "L4ki";
         repo = "Gently";
         rev = "fee7cd048197e584d0e7c44fd65776a2ff861ef4";
-        sha256 = "";
+        sha256 = "wow27jilRcaxU8277KfTY7N/WBNGS5AN3WGgPLh0Zzo=";
     };
 }
