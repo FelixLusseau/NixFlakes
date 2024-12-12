c7dabe5a9f7a6423b526a0bba26884f7cf68f125
FelixLusseau <felix.lusseau@gmail.com>
2024-12-12 22:52:26 +0100

    window decoration

--- modules/gui/home-manager/theme/gently.nix
+++ modules/gui/home-manager/theme/gently.nix
@@ -6,12 +6,12 @@ stdenv.mkDerivation rec {
     dontBuild = true;
     installPhase = ''
         mkdir -p $out/share/kwin/decorations
-        cp -aR $src/Gently\ Aurorae/ $out/share/kwin/decorations/
+        cp -aR $src/Gently\ Aurorae/Gently-Blur-Dark-Aurorae-6/ $out/share/kwin/decorations/
     '';
     src = fetchFromGitHub {
         owner = "L4ki";
         repo = "Gently";
         rev = "fee7cd048197e584d0e7c44fd65776a2ff861ef4";
-        sha256 = "070zb4ybki7509ifn6bpdp805yry9m2yi3mf99g8igs8cblyd6la";
+        sha256 = "";
     };
 }
