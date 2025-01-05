b358aa4b85920833987ad3c55990b7126b31773b
FelixLusseau <felix.lusseau@gmail.com>
2025-01-05 16:42:12 +0100

    deprecated option

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -41,7 +41,7 @@ with types;
         };
 
         # Enable sound with pipewire.
-        hardware.pulseaudio.enable = false;
+        services.pulseaudio.enable = false;
         security.rtkit.enable = true;
         services.pipewire = {
           enable = true;
