0b6c551c67af3e569fe9eb0d3ace4217d3b46f9a
FelixLusseau <felix.lusseau@gmail.com>
2025-02-09 17:10:38 +0100

    Vivid local icons

--- modules/gui/home-manager/theme/vivid-dark-icons.nix
+++ modules/gui/home-manager/theme/vivid-dark-icons.nix
@@ -1,15 +1,12 @@
-{ stdenv, fetchurl }:
+{ stdenv }:
 
 stdenv.mkDerivation rec {
     pname = "Vivid-Dark-Icons";
     version = "1.0";
-    src = fetchurl {
-        url = "file:///home/felix/Nix/NixFlakes/modules/gui/home-manager/theme/Vivid-Dark-Icons.tar.gz";
-        sha256 = "7b21e8805e1f0b632c1197208ff84e791bc47a54d1a046fbcfcf84393a971024";
-    };
+    src = ./Vivid-Dark-Icons.tar.gz;
     dontBuild = true;
     installPhase = ''
         mkdir -p $out/share/icons
-        cp -aR $src/ $out/share/icons/Vivid-Dark-Icons/
+        tar -xzf $src -C $out/share/icons/Vivid-Dark-Icons/
     '';
 }
