3a14210d87b3e59e1f4cf21d7dfa744f12fcafe9
FelixLusseau <94113911+FelixLusseau@users.noreply.github.com>
2026-02-28 11:36:29 +0100

    final version

--- modules/gui/deezer.nix
+++ modules/gui/deezer.nix
@@ -4,20 +4,19 @@
   fetchurl,
   makeWrapper,
   electron,
-  nodePackages,
 }:
 
 let
-  version = "7.1.70";
-  # nix store prefetch-file https://github.com/aunetx/deezer-linux/releases/download/v7.1.70/deezer-desktop-7.1.70-x64.tar.xz --json | jq -r .hash && nix store prefetch-file https://github.com/aunetx/deezer-linux/releases/download/v7.1.70/deezer-desktop-7.1.70-arm64.tar.xz --json | jq -r .hash
+  version = "7.1.80";
+  # nix store prefetch-file https://github.com/aunetx/deezer-linux/releases/download/v7.1.80/deezer-desktop-7.1.80-x64.tar.xz --json | jq -r .hash && nix store prefetch-file https://github.com/aunetx/deezer-linux/releases/download/v7.1.80/deezer-desktop-7.1.80-arm64.tar.xz --json | jq -r .hash
   srcs = {
     x86_64-linux = fetchurl {
       url = "https://github.com/aunetx/deezer-linux/releases/download/v${version}/deezer-desktop-${version}-x64.tar.xz";
-      hash = "sha256-N2WoG8yDFnUghDiLPXX4/I9o05h48P027Vr5N7G4k+M=";
+      hash = "sha256-3qJa62vfiwLEAzVNDQyQZew3/65T8w/ujrZ2DU7uGOA=";
     };
     aarch64-linux = fetchurl {
       url = "https://github.com/aunetx/deezer-linux/releases/download/v${version}/deezer-desktop-${version}-arm64.tar.xz";
-      hash = "sha256-6wQVBQK0hUrYCOLqQYCUdoku3Vkta9OCHf6ZUVhjtPc=";
+      hash = "sha256-qjZQJB6I7LUQhteY41Y6IBglnnJr25ApBwxvl5MUM1w=";
     };
   };
 
@@ -39,7 +38,6 @@ stdenv.mkDerivation (finalAttrs: {
 
   nativeBuildInputs = [
     makeWrapper
-    nodePackages.asar
   ];
 
   sourceRoot = ".";
@@ -54,22 +52,13 @@ stdenv.mkDerivation (finalAttrs: {
       --replace-fail "dev.aunetx.deezer" "deezer-desktop"
     cp deezer-desktop-${version}-${archDir}/resources/dev.aunetx.deezer.desktop $out/share/applications/deezer-desktop.desktop
     cp deezer-desktop-${version}-${archDir}/resources/dev.aunetx.deezer.svg $out/share/icons/hicolor/scalable/apps/deezer-desktop.svg
-
-    cp deezer-desktop-${version}-${archDir}/resources/app.asar* $out/share/deezer-desktop/resources/
-    cp -r deezer-desktop-${version}-${archDir}/resources/linux $out/share/deezer-desktop/resources/
-    cp deezer-desktop-${version}-${archDir}/resources.pak $out/share/deezer-desktop/
-
-    # # process.resourcesPath points to Electron's own resources dir when using system Electron.
-    # # Replace it with the actual app resources path directly in the bundle.
-    # asar extract "$out/share/deezer-desktop/resources/app.asar" "$TMPDIR/asar-unpacked"
-    # substituteInPlace "$TMPDIR/asar-unpacked/build/main.js" \
-    #   --replace-fail "process.resourcesPath" "\"$out/share/deezer-desktop/resources\""
-    # asar pack "$TMPDIR/asar-unpacked" "$out/share/deezer-desktop/resources/app.asar"
+    cp -r deezer-desktop-${version}-${archDir}/resources/{app.asar,linux} $out/share/deezer-desktop/resources/
 
     makeWrapper "${lib.getExe electron}" "$out/bin/deezer-desktop" \
       --inherit-argv0 \
       --add-flags "$out/share/deezer-desktop/resources/app.asar" \
-      --set-default ELECTRON_FORCE_IS_PACKAGED 1
+      --set-default ELECTRON_FORCE_IS_PACKAGED 1 \
+      --set DZ_RESOURCES_PATH "$out/share/deezer-desktop/resources"
 
     runHook postInstall
   '';
